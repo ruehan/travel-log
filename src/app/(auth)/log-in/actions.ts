3d4b79c98219908c35d30f9bbaa590317d6aee61
vendredl <node.js_@kakao.com>
2024-04-23 18:34:29 +0900

    :speech_balloon: LogIn 로직 구현

---
+++ src/app/(auth)/log-in/actions.ts
@@ -0,0 +1,69 @@
+"use server";
+
+import db from "@/app/lib/db";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import getSession from "@/app/lib/session";
+import { redirect } from "next/navigation";
+
+const checkUserExists = async (username: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			username,
+		},
+		select: {
+			id: true,
+		},
+	});
+
+	return Boolean(user);
+};
+
+const formSchema = z.object({
+	username: z
+		.string()
+		.min(3)
+		.max(10)
+		.refine(checkUserExists, "An account with this username does not exist."),
+	password: z.string({ required_error: "Password is required." }),
+});
+
+export async function LogIn(prevdata: any, formData: FormData) {
+	const data = {
+		username: formData.get("username"),
+		password: formData.get("password"),
+	};
+
+	const result = await formSchema.spa(data);
+	if (!result.success) {
+		return result.error.flatten();
+	} else {
+		const user = await db.user.findUnique({
+			where: {
+				username: result.data.username,
+			},
+			select: {
+				id: true,
+				password: true,
+			},
+		});
+		const ok = await bcrypt.compare(
+			result.data.password,
+			user!.password ?? "xxxx"
+		);
+
+		if (ok) {
+			const session = await getSession();
+			session.id = user!.id;
+			await session.save();
+			redirect("/");
+		} else {
+			return {
+				fieldErrors: {
+					password: ["Wrong password."],
+					username: [],
+				},
+			};
+		}
+	}
+}
--- src/app/(auth)/log-in/page.tsx
+++ src/app/(auth)/log-in/page.tsx
@@ -1,32 +1,52 @@
+"use client";
+
 import Link from "next/link";
+import { useFormState } from "react-dom";
+import { LogIn } from "./actions";
 
 export default function CreateAccount() {
+	const [state, dispatch] = useFormState(LogIn, null);
+
 	return (
 		<div className="flex flex-col gap-10 py-8 px-6 items-center relative h-screen">
 			<div className="absolute top-20 w-fit h-fit flex flex-col justify-center items-center gap-4">
 				<img
 					src="travelloglogo.png"
-					style={{ width: "90%", margin: "-100px" }}
+					style={{ width: "80%", margin: "-100px" }}
 				></img>
-				<div className="text-5xl font-bold">TRAVEL LOG</div>
+				<div className="text-5xl font-bold mt-[-10px]">TRAVEL LOG</div>
 			</div>
-			<form className="flex flex-col gap-3 absolute bottom-20 h-fit">
-				<div className="flex flex-col gap-2">
+			<form
+				action={dispatch}
+				className="flex flex-col gap-3 absolute bottom-10 h-fit w-[20%] min-w-[200px] justify-center items-center"
+			>
+				<div className="flex flex-col gap-3 w-full">
 					<input
+						name="username"
 						className="bg-[#dcc9ae] border-2 border-[#5a503f] text-[#5a503f] rounded-md w-full h-10  placeholder:text-[#5a503f] p-4"
 						type="text"
 						placeholder="Username"
 						required
 					/>
-					<span className="text-red-500 font-medium">Input error</span>
+					<p className="text-xs text-red-500">{state?.fieldErrors.username}</p>
+					{/* <span className="text-red-500 font-medium">Input error</span> */}
+
 					<input
+						name="password"
 						className="bg-[#dcc9ae] border-2 border-[#5a503f] text-[#5a503f] rounded-md w-full h-10  placeholder:text-[#5a503f] p-4"
-						type="text"
+						type="password"
 						placeholder="Password"
 						required
 					/>
+					<p className="text-xs text-red-500">{state?.fieldErrors.password}</p>
 				</div>
-				<button className="primary-btn h-10">LogIn</button>
+				<button className="primary-btn mt-4  bg-[#dcc9ae] border-2 border-[#5a503f] text-[#5a503f] rounded-3xl p-2 w-full">
+					LogIn
+				</button>
+				<span>OR</span>
+				<button className="primary-btn  bg-[#dcc9ae] border-2 border-[#5a503f] text-[#5a503f] rounded-3xl p-2 w-full">
+					Continue with Github
+				</button>
 			</form>
 		</div>
 	);
