487da481a4b05b37681eb7aa6bd9766f3e8f0d48
vendredl <node.js_@kakao.com>
2024-04-25 16:22:14 +0900

    :speech_balloon: 좋아요 기능 구현

--- src/app/(tabs)/page.tsx
+++ src/app/(tabs)/page.tsx
@@ -1,8 +1,10 @@
 import db from "@/app/lib/db";
 import getSession from "@/app/lib/session";
 import { redirect } from "next/navigation";
-import { Suspense } from "react";
 import moment from "moment";
+import { FaRegHeart as Heart } from "react-icons/fa";
+import { FaHeart as FillHeart } from "react-icons/fa";
+import { IoChatbubbleOutline as Bubble } from "react-icons/io5";
 
 function formatDate(date: any) {
 	const createdAt = moment(date);
@@ -32,16 +34,69 @@ async function getPost() {
 		include: {
 			user: true,
 			images: true,
+			likes: true,
+			comments: true,
+		},
+		orderBy: {
+			id: "desc",
 		},
 	});
 
 	return post;
 }
 
+function getIsLiked(likes: any, id: number) {
+	var liked = false;
+	likes.forEach((like: any) => {
+		if (like.userId === id) {
+			liked = true;
+		}
+	});
+
+	return liked;
+}
+
 async function Post() {
 	const post = await getPost();
+	const session = await getSession();
+
+	const likePost = async (formData: FormData) => {
+		"use server";
+		const session = await getSession();
+
+		const id = formData.get("id");
+
+		await db.like.create({
+			data: {
+				postId: Number(id),
+				userId: session.id!,
+			},
+		});
+
+		db.$disconnect();
+	};
+
+	const dislikePost = async (formData: FormData) => {
+		"use server";
+
+		const session = await getSession();
+
+		const id = formData.get("id");
+
+		await db.like.delete({
+			where: {
+				id: {
+					postId: Number(id),
+					userId: session.id!,
+				},
+			},
+		});
+
+		db.$disconnect();
+	};
+
 	return (
-		<div className="w-[40%] h-screen pb-[100px] flex flex-col fixed left-[50%] translate-x-[-50%] items-center  border-x-2 border-[#786657] overflow-scroll bg-[#eee6d5]">
+		<div className="w-[40%] h-screen pb-[80px] flex flex-col fixed left-[50%] translate-x-[-50%] items-center  border-x-2 border-[#786657] overflow-scroll bg-[#eee6d5]">
 			{post.map((p) => (
 				<div className="w-full h-fit flex flex-col items-center mt-4">
 					<div className="flex items-center justify-start w-[90%] gap-4">
@@ -57,12 +112,35 @@ async function Post() {
 							<div className="text-xs">{p.user.email}</div>
 						</div>
 					</div>
-					<img src={p.images[0].url} className="w-[90%] mt-4"></img>
+
+					<img src={p.images[0].url} className="w-[90%]  mt-4"></img>
+
+					<div className="flex w-[90%] mt-4 gap-2 items-center">
+						<form
+							action={getIsLiked(p.likes, session.id!) ? dislikePost : likePost}
+							className="flex items-center"
+						>
+							<input name="id" value={p.id} className="hidden" readOnly></input>
+							<button id={p.id.toString()} className="size-fit">
+								{getIsLiked(p.likes, session.id!) ? (
+									<FillHeart className="text-red-500" />
+								) : (
+									<Heart />
+								)}
+							</button>
+						</form>
+						<div className="text-xs">{p.likes.length}</div>
+						<div className="ml-4">
+							<Bubble />
+						</div>
+						<div className="text-xs">{p.comments.length}</div>
+					</div>
 
 					<div className="mt-4 flex justify-start w-[90%]">{p.content}</div>
-					<div className="mt-4 flex justify-start w-[90%] text-xs">
+					<div className="flex justify-start text-xs w-[90%] mt-4">
 						{formatDate(p.created_at)}
 					</div>
+
 					<div className="w-full h-[2px] bg-[#786657] mt-4"></div>
 				</div>
 			))}
@@ -74,8 +152,8 @@ export default async function Home() {
 	const user = await getUser();
 
 	return (
-		<Suspense fallback={"Loading"}>
-			<Post />
-		</Suspense>
+		// <Suspense fallback={"Loading"}>
+		<Post />
+		// </Suspense>
 	);
 }
