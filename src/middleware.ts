5eb44f4130190891fdf643f89527ecd0714d2b67
vendredl <node.js_@kakao.com>
2024-04-29 00:59:45 +0900

    :speech_balloon: Image URL을 위한 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,6 +25,6 @@ export async function middleware(request: NextRequest) {
 	}
 }
 
-// export const config = {
-// 	matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-// };
+export const config = {
+	matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
