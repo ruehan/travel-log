e62b617c65f2e7e7f16800b72773ea75b3c8769b
한규 <node.js_@kakao.com>
2025-02-05 11:14:15 +0900

    Delete src directory

--- src/app/(tabs)/create-post/loading.tsx
+++
@@ -1,5 +0,0 @@
-export default function Loading() {
-	return (
-		<div className="w-[40%] animate-pulse  h-screen pb-[80px] flex flex-col fixed left-[50%] translate-x-[-50%] items-center  border-x-2 border-[#786657] overflow-scroll bg-[#eee6d5]"></div>
-	);
-}
--- src/app/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import moment from "moment";
-
-export function formatDate(date: any) {
-	const createdAt = moment(date);
-	const formattedDate = createdAt.format("YYYY년 MM월 DD일 HH시 mm분");
-
-	return formattedDate;
-}
